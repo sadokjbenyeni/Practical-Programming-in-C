4a5bb470b3bac4dd8968c0e17cbc8e3b5d469fd1
Sadok Jbenyeni <sadok.jbenyeni@protonmail.com>
2023-07-19 17:09:24 +0200

    Lecture 1

---
+++ Lecture 1/problem1.3.c
@@ -0,0 +1,13 @@
+/* Outputs a simple greeting message */
+#include <stdio.h>
+
+int main(void)
+{
+    puts("Hello, 6.087 students");
+    return 0;
+}
+
+/* gcc -Wall problem1.3.c -o program.o */
+/* gdb  program.o
+   run */
+/* Hello, 6.087 students */
---
+++ Lecture 1/problem1.4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#define MSG1 "All your base are belong to us!"
+
+int main(void) {
+    const char msg[] = MSG1;
+    puts(MSG1);
+    return 0;
+}
